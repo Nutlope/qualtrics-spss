8f09084559b069c52d9038646004a61b1de02aa3
Hassan El Mghari <he55@drexel.edu>
2021-01-21 20:54:26 -0500

    changed to output spss file

--- create_SPSS_syntax.py
+++ create_SPSS_syntax.py
@@ -19,27 +19,28 @@ i = 1
 
 # Filepath to the folder with all SPSS files
 filePath = "C:\\Users\\Home\\Desktop\\Coding\\Qualtrics_SPSS_Surveys\\SPSS_Surveys"
+outputFile = open('FinalSyntax.sps','w')
 
 for key, value in surveys_dict.items():
     if key not in emptyStates:
-        print("GET")
-
-        print('  /FILE="{}\\STATE LEADER SURVEY_ Supporting Immigrant Families (2020-2021) - {}.sav".'.format(filePath, key))
-
-        print("DATASET NAME DataSet{}.".format(i))
-        print('\n')
+        outputFile.write('GET\n')
+        outputFile.write('  /FILE="{}\\STATE LEADER SURVEY_ Supporting Immigrant Families (2020-2021) - {}.sav".\n'.format(filePath, key))
+        outputFile.write("DATASET NAME DataSet{}.\n".format(i))
+        outputFile.write('\n')
         i += 1
 
-print("DATASET ACTIVATE DataSet1.")
-print("ADD FILES /FILE=*")
+outputFile.write("DATASET ACTIVATE DataSet1.\n")
+outputFile.write("ADD FILES /FILE=*\n")
 
 j = 2
 
 while j < 51 - len(emptyStates):
     if j == 50 - len(emptyStates):
-        print("  /FILE=DataSet{}.".format(j))
+        outputFile.write("  /FILE=DataSet{}.\n".format(j))
         break
-    print("  /FILE=DataSet{}".format(j))
+    outputFile.write("  /FILE=DataSet{}\n".format(j))
     j += 1
 
-print("EXECUTE.")
+outputFile.write("EXECUTE.\n")
+
+f.close()
