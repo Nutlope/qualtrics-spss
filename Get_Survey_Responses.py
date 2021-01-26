3b78ef925361e995c344293b16a64014eee2fad1
Hassan El Mghari <he55@drexel.edu>
2021-01-26 14:20:14 -0500

    deleted scheme script from get_survey_response & refactored

--- Get_Survey_Responses.py
+++ Get_Survey_Responses.py
@@ -8,8 +8,6 @@ import re
 import csv
 import constants
 
-# TODO: Delete all the crap that gets printed at the end
-
 # Create a dictionary with data from surveys.csv (user_generated_name, survey_name, survey_qualtrics_id)
 with open('surveys.csv', 'r') as f:
     reader = csv.reader(f)
@@ -31,17 +29,13 @@ def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
     downloadRequestPayload = '{"format":"' + fileFormat + '"}'
     downloadRequestResponse = requests.request("POST", baseUrl, data=downloadRequestPayload, headers=headers)
     progressId = downloadRequestResponse.json()["result"]["progressId"]
-    print(downloadRequestResponse.text)
 
     # Step 2: Checking on Data Export Progress and waiting until export is ready - GET request to qualtrics to get data
     while progressStatus != "complete" and progressStatus != "failed":
         # The result object will contain a status value. Keep looping until status has the value complete.
-
-        print("progressStatus=", progressStatus)
         requestCheckUrl = baseUrl + progressId
         requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
         requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
-        print("Download is " + str(requestCheckProgress) + " complete")
         progressStatus = requestCheckResponse.json()["result"]["status"]
 
     # step 2.1: Check for errors
@@ -51,7 +45,6 @@ def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
     # step 2.2: Get the file id
     # Once the export is complete, use the fileId field in the result object from Get Response Export Progress
     fileId = requestCheckResponse.json()["result"]["fileId"]
-    print(fileId)
 
     # Step 3: Downloading file
     requestDownloadUrl = baseUrl + fileId + '/file'
@@ -62,24 +55,7 @@ def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
     print('Complete\n')
 
 def main():
-    # Filepath to the folder where all files (CSV or SPSS) will be created
-    filePath = "C:\\Users\\Home\\Desktop\\Coding\\Qualtrics_SPSS_Surveys\\SPSS_Surveys"
-    
-    # Output file for Scheme script (ONLY RELEVANT FOR SPSS)
-    outputFile = open('FinalSyntax.sps','w')
-    
-    # Name of files created
-    surveyName = "\\STATE LEADER SURVEY_ Supporting Immigrant Families (2020-2021) - " 
-
-    i = 1
-    # Creating Scheme Script and each SPSS file
     for key, value in surveys_dict.items():
-        outputFile.write('GET\n')
-        outputFile.write('  /FILE="{}{}{}.sav".\n'.format(filePath, surveyName, key))
-        outputFile.write("DATASET NAME DataSet{}.\n".format(i))
-        outputFile.write('\n')
-        i += 1
-
         surveyId = value['survey_qualtrics_id']
         fileFormat = constants.fileFormat
         dataCenter = constants.dataCenter
@@ -96,21 +72,6 @@ def main():
             sys.exit(2)
 
         exportSurvey(apiToken, surveyId, dataCenter, fileFormat)
-
-    outputFile.write("DATASET ACTIVATE DataSet1.\n")
-    outputFile.write("ADD FILES /FILE=*\n")
-
-    j = 2
-
-    while j < 51:
-        if j == 50:
-            outputFile.write("  /FILE=DataSet{}.\n".format(j))
-            break
-        outputFile.write("  /FILE=DataSet{}\n".format(j))
-        j += 1
-
-    outputFile.write("EXECUTE.\n")
-    outputFile.close()
 
 if __name__ == "__main__":
     main()
